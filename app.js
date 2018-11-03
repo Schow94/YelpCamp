223cd74857e4b8a5e49a292a3f2add7380a09a90
Stephen <schow94@gmail.com>
2018-11-03 23:54:40 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v13Deployed", {useNewUrlParser:true});
+mongoose.connect("mongodb://stephen:kahuna94@ds151383.mlab.com:51383/yelpcampv13deployed", {useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
